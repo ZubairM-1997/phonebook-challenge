0f7bbf38354f74531570475c341f1d2e87af4872
Zubair <zubair.maqsood6@gmail.com>
2019-10-31 06:50:52 +0000

    h

--- src/ContactsList.js
+++ src/ContactsList.js
@@ -3,8 +3,11 @@ import React from 'react'
 export default class ContactsList extends React.Component{
 	render(){
 		return(
-			<input type="text" className="input" placeholder="Search..." />
-			<h1>Hello</h1>
+			<div>
+			<	input type="text" className="input" placeholder="Search..." />
+				<h1>Hello</h1>
+			</div>
+
 		)
 	}
 }
